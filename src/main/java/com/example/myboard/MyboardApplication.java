ca7a05f5f75cfc8da1cd44d5570e1351a3729dcc
jhs851 <jhs851@naver.com>
2025-02-28 01:06:35 +0900

    아 좀 되라 제발

--- src/main/java/com/example/myboard/MyboardApplication.java
+++ src/main/java/com/example/myboard/MyboardApplication.java
@@ -2,11 +2,17 @@ package com.example.myboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class MyboardApplication {
+public class MyboardApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyboardApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyboardApplication.class, args);
